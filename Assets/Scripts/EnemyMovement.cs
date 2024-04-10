11dbb76dff71a9591cf5f5219a68a77af1721ba5
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-04-10 20:55:43 +1200

    ADD:;  Enemy Picks a random location and moves towards it

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,49 +7,78 @@ public class EnemyMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Vector3 randomPosition;
-     private Vector3 fowardRight = new Vector3(1, 0, 1); 
-     private Vector3 fowardLeft = new Vector3(-1, 0, 1);
-     private Vector3 backRight = new Vector3(1, 0, -1);
-     private Vector3 backLeft = new Vector3(-1, 0, -1);
-     private Vector3 forward = new Vector3(0, 0, 1);
-     private Vector3 back = new Vector3(0, 0, -1);
-     private Vector3 right = new Vector3(1, 0, 0);
-     private Vector3 left = new Vector3(-1, 0, 0);
-     private Vector3[] directions;
-
+    private Vector3 fowardRight = new Vector3(1, 0, 1);
+    private Vector3 fowardLeft = new Vector3(-1, 0, 1);
+    private Vector3 backRight = new Vector3(1, 0, -1);
+    private Vector3 backLeft = new Vector3(-1, 0, -1);
+    private Vector3 forward = new Vector3(0, 0, 1);
+    private Vector3 back = new Vector3(0, 0, -1);
+    private Vector3 right = new Vector3(1, 0, 0);
+    private Vector3 left = new Vector3(-1, 0, 0);
+    private Vector3[] directions;
+    public bool isMoving = false;
 
     void Start()
     {
-        directions = new Vector3[8] {forward, back, right, left, fowardRight, fowardLeft, backRight, backLeft};
+        directions = new Vector3[8]
+        {
+            forward,
+            back,
+            right,
+            left,
+            fowardRight,
+            fowardLeft,
+            backRight,
+            backLeft
+        };
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
 
         agent = GetComponent<NavMeshAgent>();
-
-         randomPosition = new Vector3(Random.Range(-4, 4), 1.26f, Random.Range(-4, 4));
-        if (randomPosition != null)
-        {
-            GoToTarget(randomPosition);
-        }
+        agent.SetDestination(player.position);
     }
 
-
-// void ThrowSnowball(Vector3 direction)
-// {
-/*
-
-
-*/
-// }
+    // void ThrowSnowball(Vector3 direction)
+    // {
+    /*
+    
+    
+    */
+    // }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (!isMoving)
         {
-         randomPosition = new Vector3(Random.Range(-4, 4), 1.26f, Random.Range(-4, 4));
+            print(agent.remainingDistance);
+
+            float singleStep = 1f * Time.deltaTime;
+
+            Vector3 randomPosition = new (
+                Random.Range(-4, 4),
+                gameObject.transform.position.y,
+                Random.Range(-4, 4)
+            );
+            Vector3 targetDirection = (gameObject.transform.position - randomPosition).normalized;
+            Vector3 newDirection = Vector3.RotateTowards(
+                transform.forward,
+                targetDirection,
+                singleStep,
+                0.0f
+            );
+
+            transform.rotation = Quaternion.LookRotation(newDirection);
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+
             GoToTarget(randomPosition);
+        }
+        else if (agent.remainingDistance < 1f)
+        {
+            isMoving = false;
         }
     }
 
     private void GoToTarget(Vector3 target)
     {
         agent.SetDestination(target);
+        isMoving = true;
     }
 }
