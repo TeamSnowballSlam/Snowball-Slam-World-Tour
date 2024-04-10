2593b634e081ad17cda0a76960bc9e55c2d26273
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-04-11 11:11:19 +1200

    CHANGE: directions is now a list

--- Assets/Scripts/Directions.cs
+++ Assets/Scripts/Directions.cs
@@ -1,11 +1,12 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public static class Directions
 {
     
-    public static Vector3[] directions = new Vector3[8]
+    public static List<Vector3> directions = new()
     {
         Vector3.forward,
         Vector3.back,
