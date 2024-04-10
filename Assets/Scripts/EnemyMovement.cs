7849ecefd6e9f8ce619a30413872955f4053129d
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-04-11 09:58:16 +1200

    CHANGE: Enemy Now picks a random direction and moves to a position only if it is within the bounds of the surfacemesh

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.AI.Navigation;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,35 +7,22 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
+    public NavMeshSurface surface;
     private Vector3 randomPosition;
-    private Vector3 fowardRight = new Vector3(1, 0, 1);
-    private Vector3 fowardLeft = new Vector3(-1, 0, 1);
-    private Vector3 backRight = new Vector3(1, 0, -1);
-    private Vector3 backLeft = new Vector3(-1, 0, -1);
-    private Vector3 forward = new Vector3(0, 0, 1);
-    private Vector3 back = new Vector3(0, 0, -1);
-    private Vector3 right = new Vector3(1, 0, 0);
-    private Vector3 left = new Vector3(-1, 0, 0);
-    private Vector3[] directions;
+
     public bool isMoving = false;
     public bool isRotating = false;
+    public bool canCheckForPlayer = true;
+    public GameObject sphere;
 
     void Start()
     {
-        directions = new Vector3[8]
-        {
-            forward,
-            back,
-            right,
-            left,
-            fowardRight,
-            fowardLeft,
-            backRight,
-            backLeft
-        };
+
         Transform player = GameObject.FindGameObjectWithTag("Player").transform;
 
         agent = GetComponent<NavMeshAgent>();
+        
+
     }
 
     // void ThrowSnowball(Vector3 direction)
@@ -46,23 +34,40 @@ public class EnemyMovement : MonoBehaviour
     // }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Space pressed");
-
+        //  if (Input.GetKeyDown(KeyCode.Space))
+        //  {
+            if (GameObject.Find("TARGET") != null)    
+                Destroy(GameObject.Find("TARGET"));
             if (!isMoving)
             {
-                 randomPosition =
-                    new(Random.Range(-4, 4), gameObject.transform.position.y, Random.Range(-4, 4));
-                // RotateToTarget((transform.position -randomPosition).normalized, 50f);
-                GoToTarget(randomPosition);
+                randomPosition = transform.position + Random.Range(1, 5) * Directions.directions[Random.Range(0, 8)];
+                Bounds bounds = surface.navMeshData.sourceBounds;
+                if (randomPosition.x > bounds.min.x && randomPosition.x < bounds.max.x && randomPosition.z > bounds.min.z && randomPosition.z < bounds.max.z)
+                {
+                    GoToTarget(randomPosition);
+                    GameObject go = Instantiate(sphere, randomPosition, Quaternion.identity);
+                    go.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+                    go.name = "TARGET";
+
+                }
+                else
+                {
+                    Debug.Log("Out of bounds");
+                }
+                
+ 
             }
             else
             Debug.Log("Already moving");
-        }
-        if (Vector3.Distance(transform.position, randomPosition) < 0.01f)
+            Debug.Log(agent.remainingDistance + " " + Vector3.Distance(transform.position, randomPosition));
+        // }
+        if (Vector3.Distance(transform.position, randomPosition) < 1.5f)
             isMoving = false;
-    }
+        }
+
+
+
+    
 
     private IEnumerator RotateToTarget(Vector3 targetDirection, float speed)
     {
@@ -75,13 +80,19 @@ public class EnemyMovement : MonoBehaviour
             speed,
             0.0f
         );
-        transform.rotation = Quaternion.LookRotation(newDirection);
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+       // transform.rotation = Quaternion.LookRotation(newDirection);
+       while (Quaternion.Angle(transform.rotation, Quaternion.LookRotation(newDirection)) > 0.01)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(newDirection), 10f);
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+
+        }
         if (Quaternion.Angle(transform.rotation, Quaternion.LookRotation(newDirection)) < 0.01)
         {
             isRotating = false;
-            yield return null;
+            yield return null; 
         }
+
     }
 
     private void GoToTarget(Vector3 target)
