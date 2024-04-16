1494bbde38bc9549c33b951823385684de1aa54f
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-04-16 17:21:19 +1200

    Updated angle directions to be from a set float value

--- Assets/Scripts/Directions.cs
+++ Assets/Scripts/Directions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,17 @@ using UnityEngine.AI;
 public static class Directions
 {
     
+    private static float angleModifer = 1/Mathf.Sqrt(2);
     public static List<Vector3> directions = new()
     {
         Vector3.forward,
         Vector3.back,
         Vector3.right,
         Vector3.left,
-        Vector3.forward + Vector3.right,
-        Vector3.forward + Vector3.left,
-        Vector3.back + Vector3.right,
-        Vector3.back + Vector3.left
+       new Vector3(angleModifer,0,angleModifer),// (Vector3.forward + Vector3.right),
+       new Vector3(-angleModifer,0,angleModifer),// (Vector3.forward + Vector3.right),
+       new Vector3(angleModifer,0,-angleModifer),// (Vector3.forward + Vector3.right),
+       new Vector3(-angleModifer,0,-angleModifer),// (Vector3.forward + Vector3.right),
+
     };
 }
