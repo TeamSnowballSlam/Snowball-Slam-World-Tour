455afad7e55b60fd63004e9011fc27954fbe89d1
Erika Stuart <erikat.stuart@gmail.com>
2024-04-17 19:28:58 +1200

    Fixed limited snowball fire issue

--- Assets/Scripts/ThrowSnowballs.cs
+++ Assets/Scripts/ThrowSnowballs.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ThrowSnowballs : MonoBehaviour
 {
-    [SerializeField] private GameObject snowball;
+    [SerializeField] private GameObject snowballPrefab;
+    private GameObject snowball; // instantiate of snowballPrefab
     private Vector3 snowballPosition;
 
     // Start is called before the first frame update
@@ -16,7 +17,7 @@ public class ThrowSnowballs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) // space is player fire
         {
             ThrowSnowball();
         }
@@ -24,7 +25,8 @@ public class ThrowSnowballs : MonoBehaviour
 
     private void ThrowSnowball()
     {
-        snowball = Instantiate(this.snowball, snowballPosition + transform.forward, Quaternion.identity);
+        snowball = Instantiate(snowballPrefab, snowballPosition + transform.forward, Quaternion.identity);
+        snowball.AddComponent<SnowballCollision>();
         snowball.transform.position = snowballPosition + transform.forward;
         snowball.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         snowball.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
