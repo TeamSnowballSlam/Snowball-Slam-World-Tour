18d5cab91a866f1f26dc134b5bbc363fa731cd69
ChaseBENNC9 <BENNC9@student.op.ac.nz>
2024-04-29 10:32:34 +1200

    Snowball shooting working with players

--- Assets/Scripts/ThrowSnowballs.cs
+++ Assets/Scripts/ThrowSnowballs.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ThrowSnowballs : MonoBehaviour
 {
@@ -8,20 +9,12 @@ public class ThrowSnowballs : MonoBehaviour
     private GameObject snowball; // instantiate of snowballPrefab
     private Vector3 snowballPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q)) // wasd player fire key is q
-        {
-            ThrowSnowball();
-        }
-    }
+   
 
-    private void ThrowSnowball()
+    public void ThrowSnowball(InputAction.CallbackContext context)
     {
         snowballPosition = new Vector3(transform.position.x, 1.5f, transform.position.z); // thrown at face level
         snowball = Instantiate(snowballPrefab, snowballPosition + transform.forward, Quaternion.identity); // snowballPrefab is instantiated
-        snowball.transform.position = snowballPosition + transform.forward; // snowball is positioned in front of player
         snowball.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse); // snowball moves at a constant rate
     }
 }
