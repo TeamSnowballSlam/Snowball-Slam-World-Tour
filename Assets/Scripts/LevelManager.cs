f5bcab4d249a682e53a6eebdc095667524df285f
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-05-01 21:46:33 +1200

    add: Added a static instance to LevelManager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -21,7 +21,19 @@ public class LevelManager : MonoBehaviour
     private TextMeshProUGUI timerText;
     private float currentTime;
    
+    public static LevelManager instance;
 
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
