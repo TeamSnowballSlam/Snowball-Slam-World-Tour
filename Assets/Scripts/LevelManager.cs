f44dbe5d579dffcdea0c56ffea14e325038a5b96
ChaseBENNC9 <BENNC9@student.op.ac.nz>
2024-05-02 07:25:27 +1200

    doc: Added Comments to LevelManager Script

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,32 @@
+/// <remarks>
+/// Author: Chase Bennett-Hill
+/// Date Created: May 2, 2024
+/// Bugs: None known at this time.
+/// </remarks>
+// <summary>
+/// This class manages the level, it will keep track of each teams score , the time ramaining and the current state of the level
+/// </summary>
+
+
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
+    [Header("Level Management")]
+    [SerializeField]
+    private int playerScore = 0;
 
-    [SerializeField] private int currentScore = 0;
-    [SerializeField] private int enemyScore = 0;
-    [SerializeField] private int secondsRemaining = 60;
+    [SerializeField]
+    private int enemyScore = 0;
 
+    [SerializeField]
+    private int secondsRemaining = 60;
+
+    [Header("Colors")]
     public Color mediumColor;
     public Color criticalColor;
     private bool roundOver = false;
@@ -20,12 +36,12 @@ public class LevelManager : MonoBehaviour
     private TextMeshProUGUI enemyScoreText;
     private TextMeshProUGUI timerText;
     private float currentTime;
-   
+
     public static LevelManager instance;
 
     void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
         }
@@ -42,19 +58,22 @@ public class LevelManager : MonoBehaviour
         enemyScoreText = GameObject.Find("EnemyScore").GetComponent<TextMeshProUGUI>();
         timerText = GameObject.Find("LevelTimer").GetComponent<TextMeshProUGUI>();
 
-        playerScoreText.text =  currentScore.ToString();
+        playerScoreText.text = playerScore.ToString();
         enemyScoreText.text = enemyScore.ToString();
 
         //Format the time to display as MM:SS
-        string formattedTime = string.Format("{0:00}:{1:00}", secondsRemaining / 60, secondsRemaining % 60);
+        string formattedTime = string.Format(
+            "{0:00}:{1:00}",
+            secondsRemaining / 60,
+            secondsRemaining % 60
+        );
         timerText.text = formattedTime;
         currentTime = Time.time;
-
     }
 
     void Update()
     {
-        if(!timerStarted)
+        if (!timerStarted)
         {
             StartCountdown();
         }
@@ -63,17 +82,23 @@ public class LevelManager : MonoBehaviour
             //Update the timer every second if the round is not over
             if (Time.time > currentTime + 1 && !roundOver)
             {
-                if(secondsRemaining > 0)
+                if (secondsRemaining > 0)
                 {
                     currentTime = Time.time;
                     secondsRemaining -= 1;
-                    string formattedTime = string.Format("{0:00}:{1:00}", secondsRemaining / 60, secondsRemaining % 60);
+                    string formattedTime = string.Format(
+                        "{0:00}:{1:00}",
+                        secondsRemaining / 60,
+                        secondsRemaining % 60
+                    );
                     timerText.text = formattedTime;
-                    if(secondsRemaining <= 5)
+
+                    //Once the timer reaches either threshold, change the color of the text
+                    if (secondsRemaining <= 5)
                     {
                         timerText.color = criticalColor;
                     }
-                    else if(secondsRemaining <= 10)
+                    else if (secondsRemaining <= 10)
                     {
                         timerText.color = mediumColor;
                     }
@@ -86,26 +111,31 @@ public class LevelManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// This method will start the countdown timer
+    /// </summary>
     private void StartCountdown()
     {
         timerStarted = true;
     }
 
+    /// <summary>
+    /// This method will update the score of the team that scored
+    /// </summary>
+    /// <param name="team">The name of the team which will be given points</param>
     public void UpdateScore(string team)
     {
-        if(team == "Player")
+        if (team == "Player")
         {
-            currentScore += 1;
-            playerScoreText.text = currentScore.ToString();
+            playerScore += 1;
+            playerScoreText.text = playerScore.ToString();
         }
-        else if(team == "Enemy")
+        else if (team == "Enemy")
         {
             enemyScore += 1;
             enemyScoreText.text = enemyScore.ToString();
         }
-        else return;
+        else
+            return;
     }
-    
-
-
 }
