5c009610c1ac41f61a3db1da8b46452d613f41bc
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-05-02 10:17:12 +1200

    Snowballs no longer go into the negatives

--- Assets/Scripts/Snowball Scripts/ThrowSnowballs.cs
+++ Assets/Scripts/Snowball Scripts/ThrowSnowballs.cs
@@ -20,6 +20,7 @@ public class ThrowSnowballs : MonoBehaviour
 
     public void ThrowSnowball(InputAction.CallbackContext context)
     {
+        if (snowInventory.currentAmmo <= 0) return; // if no ammo, don't throw snowball
         if (context.phase != InputActionPhase.Started) return; // only throw snowball once--when phase is started
         snowballPosition = new Vector3(transform.position.x, 1.5f, transform.position.z); // thrown at face level
         snowball = Instantiate(
@@ -33,13 +34,13 @@ public class ThrowSnowballs : MonoBehaviour
 
     public void ThrowSnowball()
     {
-            snowballPosition = new Vector3(transform.position.x, 1.5f, transform.position.z); // thrown at face level
-            snowball = Instantiate(
-                snowballPrefab,
-                snowballPosition + transform.forward,
-                Quaternion.identity
-            ); // snowballPrefab is instantiated
-            snowball.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse); // snowball moves at a constant rate
-        //}
+        snowballPosition = new Vector3(transform.position.x, 1.5f, transform.position.z); // thrown at face level
+        snowball = Instantiate(
+            snowballPrefab,
+            snowballPosition + transform.forward,
+            Quaternion.identity
+        ); // snowballPrefab is instantiated
+        snowball.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse); // snowball moves at a constant rate
+        
     }
 }
