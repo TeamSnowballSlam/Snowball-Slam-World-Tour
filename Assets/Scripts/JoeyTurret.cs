ec836ed3cfa67235a2fd88f9dd8233b5963b96ed
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-05-07 18:22:58 +1200

    add: created duplicate of joey prefab

---
+++ Assets/Scripts/JoeyTurret.cs
@@ -0,0 +1,27 @@
+/// <remarks>
+/// Author: Chase Bennett-Hill
+/// Date Created: May 7, 2024
+/// Bugs: None known at this time.
+/// </remarks>
+// <summary>
+///Manages the Joey Turret Object
+////// </summary>
+using UnityEngine;
+
+public class JoeyTurret : MonoBehaviour
+{
+    public int fireRate; //The rate of fire for the ability
+    public float expireTime = 15f; //The time before the ability expires
+    public GameObject parent; //The parent object of the ability
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
