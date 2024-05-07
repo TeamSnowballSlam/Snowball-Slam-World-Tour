3ac54559731dbf838ee1f4ba1dcbcecb2e99a06a
ChaseBENNC9 <BENNC9@student.op.ac.nz>
2024-05-07 14:47:58 +1200

    add: create joey class

---
+++ Assets/Scripts/JoeyAbility.cs
@@ -0,0 +1,31 @@
+/// <remarks>
+/// Author: Chase Bennett-Hill
+/// Date Created: May 7, 2024
+/// Bugs: None known at this time.
+/// </remarks>
+// <summary>
+///This class handles the joey summoning ability for the kangaroo enemy
+////// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoeyAbility : MonoBehaviour
+{
+    public int fireRate; //The rate of fire for the ability
+    public float expireTime = 15f; //The time before the ability expires
+    public GameObject joeyPrefab; //The joey prefab
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
