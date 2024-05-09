6d873cb5d8bb3fd4d848574ce0893684156cc99f
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-05-09 13:22:42 +1200

    Slider finding

--- Assets/Scripts/Snowball Scripts/ReloadAmmo.cs
+++ Assets/Scripts/Snowball Scripts/ReloadAmmo.cs
@@ -1,40 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ReloadAmmo : MonoBehaviour
 {
     private SnowInventory snowInventory;
+    private bool canReload = false;
+    private GameObject snowPile;
+    [SerializeField]private GameObject reloadSlider;
+    private double timeHeld;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        snowPile = GameObject.Find("Player Snow Pile");
+        reloadSlider = snowPile.transform.Find("Canvas/Progress").gameObject;
+        reloadSlider.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "Player Snow Pile")
         {
-            Debug.Log("Entered reload area");
-            snowInventory = GetComponent<SnowInventory>();
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-                StartCoroutine(HoldReload());
-            }
+            canReload = true;
         }
     }
 
-    private IEnumerator HoldReload()
+    void onTriggerExit(Collider other)
     {
-        yield return new WaitForSeconds(3);
-        snowInventory.currentAmmo += 1;
-        Debug.Log("Updated snowballs: " + snowInventory.currentAmmo);
+        if(other.gameObject.name == "Player Snow Pile")
+        {
+            canReload = false;
+        }
+    }
+
+    public void OnHold(InputAction.CallbackContext context)
+    {
+        if (canReload)
+        {
+            reloadSlider.SetActive(true);
+            timeHeld = context.time;
+            reloadSlider.GetComponent<Slider>().value = (float)context.time;
+        }
     }
 }
