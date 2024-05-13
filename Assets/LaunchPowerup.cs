eda0ef5e2296303af65687e1e4a0d3523eec92d9
ChaseBENNC9 <BENNC9@student.op.ac.nz>
2024-05-13 16:30:39 +1200

    change: renamed script to LaunchPowerup

--- Assets/LaunchPowerup.cs
+++ Assets/LaunchPowerup.cs
@@ -12,7 +12,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Powerup : MonoBehaviour
+public class LaunchPowerup : MonoBehaviour
 {   /**
         Kinematic equations:
         1. s = ((u + v) * t) / 2
@@ -27,51 +27,57 @@ public class Powerup : MonoBehaviour
         s = displacement
         t = duration time
      */
- 
+
     public new Rigidbody rigidbody;
-    public Transform target;
+    private Transform target;
     public Vector3 InitialVelocity;
- 
+
     public float maximumHeightOfArc;
     public float gravity;
     public int pathResolution;
     public bool showDebugPath;
- 
+
     private bool isLaunching;
     private Vector3 savedPosition;
- 
-    struct LaunchData {
+
+    struct LaunchData
+    {
         public readonly Vector3 initialVelocity;
         public readonly float durationTime;
- 
-        public LaunchData(Vector3 velocity, float time) {
+
+        public LaunchData(Vector3 velocity, float time)
+        {
             this.initialVelocity = velocity;
             this.durationTime = time;
         }
     }
- 
-    LaunchData CalculateLaunchData() {
+
+    LaunchData CalculateLaunchData()
+    {
         float displacementY = target.position.y - rigidbody.position.y;
         Vector3 displacementXZ = new Vector3(target.position.x - rigidbody.position.x, 0, target.position.z - rigidbody.position.z);
- 
+
         Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * maximumHeightOfArc);
-        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * maximumHeightOfArc / gravity) + Mathf.Sqrt(2 * (displacementY - maximumHeightOfArc)/gravity));
- 
+        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * maximumHeightOfArc / gravity) + Mathf.Sqrt(2 * (displacementY - maximumHeightOfArc) / gravity));
+
         float time = Mathf.Sqrt(-2 * maximumHeightOfArc / gravity) + Mathf.Sqrt(2 * (displacementY - maximumHeightOfArc) / gravity);
- 
+        Debug.Log("DisplacementY: " + displacementY + " DisplacementXZ: " + displacementXZ + " VelocityY: " + velocityY + " VelocityXZ: " + velocityXZ + " Time: " + time);
+
         return new LaunchData(velocityXZ + velocityY * -Mathf.Sign(gravity), time);
     }
- 
-    void Launch() {
+
+    void Launch()
+    {
         this.isLaunching = true;
         this.savedPosition = rigidbody.position;
         Physics.gravity = Vector3.up * this.gravity;
         rigidbody.useGravity = true;
- 
+
         LaunchData data = CalculateLaunchData();
         if (!float.IsNaN(data.initialVelocity.y) && !float.IsInfinity(data.initialVelocity.y))
             rigidbody.velocity = data.initialVelocity;
-        else {
+        else
+        {
             this.isLaunching = false;
             rigidbody.useGravity = false;
             rigidbody.position = new Vector3(Random.insideUnitCircle.x * Random.Range(-100, 100), 0, Random.insideUnitCircle.y * Random.Range(-100, 100));
@@ -79,48 +85,68 @@ public class Powerup : MonoBehaviour
             rigidbody.velocity = Vector3.zero;
         }
     }
- 
-    void DrawPath() {
+
+    void DrawPath()
+    {
         LaunchData launchData = CalculateLaunchData();
-        if (float.IsNaN(launchData.initialVelocity.y) || float.IsInfinity(launchData.initialVelocity.y)) {
+        if (float.IsNaN(launchData.initialVelocity.y) || float.IsInfinity(launchData.initialVelocity.y))
+        {
             return;
         }
         Vector3 originalPosition = rigidbody.position;
- 
+
         Vector3[] positions = new Vector3[this.pathResolution + 1];
-        for (int i = 0; i <= this.pathResolution; i++) {
-            float simulationTime = (i / (float) this.pathResolution) * launchData.durationTime;
+        for (int i = 0; i <= this.pathResolution; i++)
+        {
+            float simulationTime = (i / (float)this.pathResolution) * launchData.durationTime;
             Vector3 displacement = launchData.initialVelocity * simulationTime + (Vector3.up * gravity) * simulationTime * simulationTime / 2f;
             positions[i] = originalPosition + displacement;
         }
 
- 
+
         this.InitialVelocity = launchData.initialVelocity;
     }
- 
-    void Awake() {
+
+    void Awake()
+    {
         this.rigidbody = GetComponent<Rigidbody>();
     }
- 
-    void Start() {
+
+    void Start()
+    {
         rigidbody.useGravity = false;
         this.isLaunching = false;
+        Bounds bounds = LevelManager.instance.bounds;
+        float randomX = Random.Range(bounds.min.x, bounds.max.x);
+        float randomZ = Random.Range(bounds.min.z, bounds.max.z);
+        Debug.Log("RandomX: " + randomX + " RandomZ: " + randomZ);
+        Vector3 randomPosition = new Vector3(randomX, 0, randomZ);
+        GameObject targetObject = new GameObject("Target");
+        targetObject.transform.position = randomPosition;
+        this.target = targetObject.transform;
+        this.maximumHeightOfArc = 10;
+        this.gravity = -9.81f;
+        Debug.Log("Target Position: " + this.target.position);
     }
- 
-    void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            DrawPath();
-            Launch();
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                DrawPath();
+                Launch();
+            }
+            if (showDebugPath && !this.isLaunching)
+            {
+                DrawPath();
+            }
         }
-        if (showDebugPath && !this.isLaunching) {
-            DrawPath();
-        }  
-    }
- 
-    void OnValidate() {
-        if (showDebugPath && !this.isLaunching) {
-            DrawPath();
-        }  
-    }
 
-}
+        void OnValidate()
+        {
+            if (showDebugPath && !this.isLaunching)
+            {
+                DrawPath();
+            }
+        }
+
+    }
