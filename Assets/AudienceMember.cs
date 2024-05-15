244b047603a845144884b5b5a58b7f93f6697897
ChaseBENNC9 <BENNC9@student.op.ac.nz>
2024-05-16 09:14:28 +1200

    doc: added explaining comments

--- Assets/AudienceMember.cs
+++ Assets/AudienceMember.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    public enum PowerUpType
-    {
-        Speed,
-        Shield,
-        SnowballLauncher,
-        Unassigned
-    }
+
+/// <summary>
+/// The Different types of power ups that can be thrown
+/// </summary>
+public enum PowerUpType
+{
+    Speed, //The speed boost (fish)
+    Shield, //Protects from 3 hits (egg hat)
+    SnowballLauncher, //Enhanced throwing rate
+    Unassigned //No power up assigned
+}
 
 public class AudienceMember : MonoBehaviour
 {
     public PowerUpType powerUpType;
 
     [Range(0, 100)]
-    public int powerUpSpawnChance;
-    private const int POWERUPDELAY = 5;
+    [SerializeField]
+    /// <summary>
+    /// The chance of a power up being thrown
+    /// </summary>
+    private int powerUpSpawnChance;
+
+    [SerializeField]
+    /// <summary>
+    /// The time in seconds between each possible power up spawn
+    /// </summary>
+    private int throwFrequency = 5;
+    /// <summary>
+    /// The last time a power up was spawned
+    /// </summary>
     private float lastSpawnTime = 0;
 
-    [SerializeField] private GameObject powerUpPrefab;
+    [SerializeField]
+    private GameObject powerUpPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,11 +46,11 @@ public class AudienceMember : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time - lastSpawnTime > POWERUPDELAY)
+        if (Time.time - lastSpawnTime > throwFrequency)
         {
             Debug.Log("Spawning power up");
             lastSpawnTime = Time.time;
-            if(Random.Range(0, 100) < powerUpSpawnChance)
+            if (Random.Range(0, 100) < powerUpSpawnChance)
             {
                 PickPowerUp();
                 SpawnPowerUp();
@@ -60,6 +78,7 @@ public class AudienceMember : MonoBehaviour
                 return "Unknown";
         }
     }
+
     /// <summary>
     /// Picks a random power up type
     /// </summary>
@@ -67,7 +86,6 @@ public class AudienceMember : MonoBehaviour
     {
         int random = Random.Range(0, 3);
         powerUpType = (PowerUpType)random;
-        Debug.Log("Picked power up: " + ParsePowerUpType());
     }
 
     private void SpawnPowerUp()
