aa1e73851a944c76bc14826f07a2fe5625a80475
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-05-27 15:38:01 +1200

    Changes needed to push

--- Assets/Scripts/UI/PauseSettings.cs
+++ Assets/Scripts/UI/PauseSettings.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class PauseSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     /// <summary>
     /// When the music slider is changed
     /// </summary>
